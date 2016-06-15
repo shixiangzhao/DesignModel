5c8c4c5f8f953411e54df8e2392a624c37a9eb25
shixiang.zhao <shixiang.zhao@cienet.com.cn>
2016-06-15 10:23:26 +0800

    update command

--- src/com/cienet/command/Client.java
+++ src/com/cienet/command/Client.java
@@ -1,5 +1,11 @@
 package com.cienet.command;
-
+/**
+ * 烤羊肉串引发的思考：
+ * 命令模式，将一个请求封装成为一个对象，从而使你可用不同的请求对客户进行参数化；
+ * 对请求排队或记录请求日志，以及支持可撤销的操作。
+ * @author new
+ *
+ */
 public class Client {
 
     public static void main(String[] args) {
@@ -10,10 +16,10 @@ public class Client {
         Waiter girl = new Waiter();
         
         girl.setOrder(bakeChickenWingCommand1);
-        girl.Notify();
         girl.setOrder(bakeMuttonCommand1);
-        girl.Notify();
         girl.setOrder(bakeMuttonCommand2);
+        girl.cancelOrder(bakeChickenWingCommand1);
+        
         girl.Notify();
     }
 
--- src/com/cienet/command/Waiter.java
+++ src/com/cienet/command/Waiter.java
@@ -1,14 +1,32 @@
 package com.cienet.command;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 public class Waiter {
     
-    private Command command;
+    private List<Command> orders = new ArrayList<Command>();
     
     public void setOrder(Command command){
-        this.command = command;
+        if(command.toString() == "meiyyou"){
+            System.out.println("服务员：鸡翅没有了，请点别的烧烤！");
+        }else{
+            orders.add(command);
+            System.out.println("增加订单： " + command.toString() + 
+                " 时间：" + new Date().toString());
+        }
+    }
+    
+    public void cancelOrder(Command command){
+        orders.remove(command);
+        System.out.println("取消订单： " + command.toString() + 
+            " 时间：" + new Date().toString());
     }
     
     public void Notify(){
-        command.excuteCommand();
+        for(Command cmd : orders){
+            cmd.excuteCommand();
+        }
     }
 }
